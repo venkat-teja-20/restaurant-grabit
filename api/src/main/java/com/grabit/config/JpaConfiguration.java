555d58d6e26adbeefef78f36ac84ec3ded9f06d0
Sai Venkata Teja <saivenkatateja714@gmail.com>
2025-12-27 20:53:02 +0530

    auditorAware modified to capture the audit person email

--- api/src/main/java/com/grabit/config/JpaConfiguration.java
+++ api/src/main/java/com/grabit/config/JpaConfiguration.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -14,6 +19,15 @@ public class JpaConfiguration {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("System");
+        return ()->{
+            Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+            Object principal=authentication.getPrincipal();
+            if(principal instanceof UserDetails userDetails)
+                return Optional.ofNullable(userDetails.getUsername());
+            else if(principal instanceof String)
+                return Optional.of(String.valueOf(principal));
+            else
+                return Optional.of("Anonymous User");
+        };
     }
 }
